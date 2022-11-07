9663e14e22a9d7f5371787c6c62280512e407b47
TIENDAT <dathee93@gmail.com>
2022-11-07 16:10:19 +0700

    component

--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.scss';
+import MyComponent from './Example/MyComponent';
 
 function App() {
+  // 1 component của react là hàm, class hay funcion, mà nó trả ra 1 khối html
+  // jsx biến là thẻ html
+  // class component : bắt buộc dùng hàng render
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HELLO <code>WWORLD</code>
         </p>
         <a
           className="App-link"
@@ -17,6 +22,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyComponent></MyComponent>
       </header>
     </div>
   );
---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>Hello Component</div>
+        )
+    }
+}
+
+export default MyComponent;
+
+// Tổng kết bài học: là 1 class, 1 fuction, hay 1 hàm có chức năng đặc biệt là render html
+// Component của React có 1 điều đb là sử dụng cú pháp JSX là funcion, hàm của javascript trả về 1 đống html thay vì trả về 1 biến
