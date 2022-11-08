632979b5238be3652e1c6a343c3c24748d0c35dc
TIENDAT <dathee93@gmail.com>
2022-11-08 08:21:54 +0700

    new

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    // JSX => Return Block
+
     render() {
+        let name = 'dathee'
         return (
-            <div>Hello Component</div>
+            <React.Fragment>
+                <div>
+                    Hello Component, my name is {name}
+                </div>
+            </React.Fragment>
         )
     }
 }
@@ -12,3 +20,4 @@ export default MyComponent;
 
 // Tổng kết bài học: là 1 class, 1 fuction, hay 1 hàm có chức năng đặc biệt là render html
 // Component của React có 1 điều đb là sử dụng cú pháp JSX là funcion, hàm của javascript trả về 1 đống html thay vì trả về 1 biến
+// Muốn dùng console.log trong html chỉ cần sử dụng dấu {}
